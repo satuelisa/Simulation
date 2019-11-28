519a30387723989569965df527bb17b4ff575506
SatuElisa <elisa.schaeffer@gmail.com>
2019-11-28 09:42:04 -0600

    Execution time for primality testing
    Measuring the execution time of primality testing with the test data ordered in three different ways

---
+++ QueuingTheory/ordering.R
@@ -0,0 +1,34 @@
+primo <- function(n) {
+    if (n == 1 || n == 2) {
+        return(TRUE)
+    }
+    if (n %% 2 == 0) {
+        return(FALSE)
+    }
+    for (i in seq(3, max(3, ceiling(sqrt(n))), 2)) {
+        if ((n %% i) == 0) {
+            return(FALSE)
+        }
+    }
+    return(TRUE)
+}
+ 
+desde <- 1000
+hasta <-  3000
+original <- desde:hasta
+invertido <- hasta:desde
+replicas <- 10
+suppressMessages(library(doParallel))
+registerDoParallel(makeCluster(detectCores() - 1))
+ot <-  numeric()
+it <-  numeric()
+at <-  numeric()
+for (r in 1:replicas) {
+    ot <- c(ot, system.time(foreach(n = original, .combine=c) %dopar% primo(n))[3]) # de menor a mayor
+    it <- c(it, system.time(foreach(n = invertido, .combine=c) %dopar% primo(n))[3]) # de mayor a menor
+    at <- c(at, system.time(foreach(n = sample(original), .combine=c) %dopar% primo(n))[3]) # orden aleatorio
+}
+stopImplicitCluster()
+summary(ot)
+summary(it)
+summary(at)
