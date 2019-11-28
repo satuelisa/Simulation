26d1cc6a9b7aaf782f089cab4842cb5228bb617b
SatuElisa <elisa.schaeffer@gmail.com>
2019-11-28 10:39:36 -0600

    Auxiliary routines related to Knapsack in Python

---
+++ GeneticAlgorithm/routines.py
@@ -0,0 +1,57 @@
+import numpy as np
+ 
+def knapsack(peso_permitido, pesos, valores):
+    assert len(pesos) == len(valores)
+    peso_total = sum(pesos)
+    valor_total = sum(valores)
+    if peso_total < peso_permitido: 
+        return valor_total
+    else:
+        V = dict()
+        for w in range(peso_permitido + 1):
+            V[(w, 0)] = 0
+        for i in range(len(pesos)):
+            peso = pesos[i]
+            valor = valores[i]
+            for w in range(peso_permitido + 1):
+                cand = V.get((w - peso, i), -float('inf')) + valor
+                V[(w, i + 1)] = max(V[(w, i)], cand)
+        return max(V.values())
+ 
+def factible(seleccion, pesos, capacidad):
+    return np.inner(seleccion, pesos) <= capacidad
+  
+def objetivo(seleccion, valores):
+    return np.inner(seleccion, valores)
+ 
+def al_azar(cuantos):
+    return np.round(np.random.uniform(size = cuantos))
+  
+def normalizar(data):
+    menor = min(data)
+    mayor = max(data)
+    rango  = mayor - menor
+    data = data - menor # > 0
+    return data / rango # entre 0 y 1
+  
+def generador_pesos(cuantos, low, high):
+    return np.round(normalizar(np.random.normal(size = cuantos)) * (high - low) + low)
+ 
+from random import random
+def generador_valores(pesos, low, high):
+    n = len(pesos)
+    valores = np.empty((n))
+    for i in range(n):
+        valores[i] = np.random.normal(pesos[i], random(), 1)
+    return normalizar(valores) * (high - low) + low
+ 
+n = 20
+pesos = generador_pesos(n, 15, 80)
+valores = generador_valores(pesos, 10, 500)
+capacidad = int(round(sum(pesos) * 0.65))
+print(pesos)
+print(valores)
+print(knapsack(capacidad, pesos, valores))
+for i in range(10):
+    intento = al_azar(n)
+    print((factible(intento, pesos, capacidad), objetivo(intento, valores)))
