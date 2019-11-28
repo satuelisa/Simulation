79fdb8a0827baf0f9ad105c82ce4f6c5583d556a
SatuElisa <elisa.schaeffer@gmail.com>
2019-11-28 09:47:31 -0600

    Cálculo de las celdas Voronoi en R
    Cada celda de la rejilla se asigna a la celda Voronoi de la semilla más cercada en términos de la distancia Euclideana

---
+++ VoronoiDiagrams/cells.R
@@ -0,0 +1,54 @@
+n <-  40
+zona <- matrix(rep(0, n * n), nrow = n, ncol = n)
+k <- 12
+x <- rep(0, k) # ocupamos almacenar las coordenadas x de las semillas
+y <- rep(0, k) # igual como las coordenadas y de las semillas
+ 
+for (semilla in 1:k) {
+    while (TRUE) { # hasta que hallamos una posicion vacia para la semilla
+        fila <- sample(1:n, 1)
+        columna <- sample(1:n, 1)
+        if (zona[fila, columna] == 0) {
+            zona[fila, columna] = semilla
+            x[semilla] <- columna
+            y[semilla] <- fila
+            break
+        }
+    }
+}
+ 
+celda <-  function(pos) {
+    fila <- floor((pos - 1) / n) + 1
+    columna <- ((pos - 1) %% n) + 1
+    if (zona[fila, columna] > 0) { # es una semilla
+        return(zona[fila, columna])
+    } else {
+        cercano <- NULL # sin valor por el momento
+        menor <- n * sqrt(2) # mayor posible para comenzar la busqueda
+        for (semilla in 1:k) {
+            dx <- columna - x[semilla]
+            dy <- fila - y[semilla]
+            dist <- sqrt(dx^2 + dy^2)
+            if (dist < menor) {
+                cercano <- semilla
+                menor <- dist
+            }
+        }
+        return(cercano)
+    }
+}
+ 
+suppressMessages(library(doParallel))
+registerDoParallel(makeCluster(detectCores() - 1))
+celdas <- foreach(p = 1:(n * n), .combine=c) %dopar% celda(p)
+stopImplicitCluster()
+voronoi <- matrix(celdas, nrow = n, ncol = n, byrow=TRUE)
+rotate <- function(x) t(apply(x, 2, rev))
+png("p4s.png")
+par(mar = c(0,0,0,0))
+image(rotate(zona), col=rainbow(k+1), xaxt='n', yaxt='n')
+graphics.off()
+png("p4c.png")
+par(mar = c(0,0,0,0))
+image(rotate(voronoi), col=rainbow(k+1), xaxt='n', yaxt='n')
+graphics.off()
