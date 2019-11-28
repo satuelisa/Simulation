059cad2ae8be836bb0aaf63ec40b33cbae7d210d
SatuElisa <elisa.schaeffer@gmail.com>
2019-11-28 09:48:35 -0600

    Voronoi cell computation in Python

---
+++ VoronoiDiagrams/cells.py
@@ -0,0 +1,47 @@
+import seaborn as sns 
+from random import randint
+from PIL import Image, ImageColor 
+ 
+n = 40
+k = 12
+semillas = []
+for s in range(k):
+    while True:
+        x = randint(0, n - 1)
+        y = randint(0, n - 1)
+        if (x, y) not in semillas:
+            semillas.append((x, y))
+            break
+ 
+from math import sqrt
+def celda(pos):
+    y = pos // n
+    x = pos % n
+    if pos in semillas:
+        return semillas.index(pos)
+    cercano = None
+    menor = n * sqrt(2)
+    for i in range(k):
+        (xs, ys) = semillas[i]
+        dx = x - xs
+        dy = y - ys
+        dist = sqrt(dx**2 + dy**2)
+        if dist < menor:
+            cercano = i
+            menor = dist
+    return cercano
+ 
+import multiprocessing
+if __name__ == "__main__":
+    celdas = None
+    with multiprocessing.Pool() as pool:
+        celdas = pool.map(celda, range(n * n))
+        zona = Image.new('RGB', (n, n))
+        p = zona.load()
+        c = sns.color_palette("Set3", k).as_hex()
+        for i in range(n * n):
+            s = celdas.pop(0)
+            p[i % n, i // n] = ImageColor.getrgb(c[s])
+    visual = zona.resize((10 * n,  10 * n))
+    visual.show()
+    visual.save("p4pc.png")
