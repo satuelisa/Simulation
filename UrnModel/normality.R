8572c9645b0a31eeaa7aa6c13606b846884ea4ca
SatuElisa <elisa.schaeffer@gmail.com>
2019-11-28 10:16:32 -0600

    Test for normality

---
+++ UrnModel/normality.R
@@ -0,0 +1,31 @@
+k <- 1000
+n <- 100000
+originales <- rnorm(k)
+cumulos <- originales - min(originales)  + 1
+cumulos <- round(n * cumulos / sum(cumulos))
+cumulos <- round(cumulos) 
+diferencia <- n - sum(cumulos) 
+if (diferencia > 0) { 
+    for (i in 1:diferencia) {
+        p <- sample(1:k, 1) 
+        cumulos[p] <- cumulos[p] + 1 
+    }
+} else if (diferencia < 0) { 
+    for (i in 1:-diferencia) {
+        p <- sample(1:k, 1) 
+        if (cumulos[p] > 1) { 
+            cumulos[p] <- cumulos[p] - 1 
+        }
+    }
+}
+png("p8_norm.png")
+par(mfrow = c(2, 2)) # juntamos graficas
+plot(density(originales)) # lo generado que era normal
+print(shapiro.test(originales))
+qqnorm(originales)
+qqline(originales, col = 2)
+plot(density(cumulos)) # lo nuestro que hemos modificado
+print(shapiro.test(cumulos))
+qqnorm(cumulos)
+qqline(cumulos, col = 2)
+graphics.off()
