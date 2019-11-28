9b25bef61504089046b645304c633453e7bf90a7
SatuElisa <elisa.schaeffer@gmail.com>
2019-11-28 10:50:16 -0600

    Optimal solutions for each objetive

---
+++ ParetoFronts/opt.R
@@ -0,0 +1,60 @@
+pick.one <- function(x) {
+    if (length(x) == 1) {
+        return(x)
+    } else {
+        return(sample(x, 1))
+    }
+}
+ 
+poli <- function(maxdeg, varcount, termcount) {
+    f <- data.frame(variable=integer(), coef=integer(), degree=integer())
+    for (t in 1:termcount) {
+        var <- pick.one(1:varcount)
+        deg <- pick.one(1:maxdeg)
+        f <-  rbind(f, c(var, runif(1), deg))
+    }
+    names(f) <- c("variable", "coef", "degree")
+    return(f)
+}
+ 
+eval <- function(pol, vars, terms) {
+    value <- 0.0
+    for (t in 1:terms) {
+        term <- pol[t,]
+        value <-  value + term$coef * vars[term$variable]^term$degree
+    }
+    return(value)
+}
+ 
+vc <- 4
+md <- 3
+tc <- 5
+k <- 2 # cuantas funciones objetivo
+obj <- list()
+for (i in 1:k) {
+    obj[[i]] <- poli(md, vc, tc)
+}
+minim <- (runif(k) > 0.5)
+n <- 250 # cuantas soluciones aleatorias
+sol <- matrix(runif(vc * n), nrow=n, ncol=vc)
+val <- matrix(rep(NA, k * n), nrow=n, ncol=k)
+for (i in 1:n) { # evaluamos las soluciones
+    for (j in 1:k) { # para todos los objetivos
+        val[i, j] <- eval(obj[[j]], sol[i,], tc)
+    }
+}
+mejor1 <- which.max((1 + (-2 * minim[1])) * val[,1])
+mejor2 <- which.max((1 + (-2 * minim[2])) * val[,2])
+cual <- c("max", "min")
+xl <- paste("Primer objetivo (", cual[minim[1] + 1], ")", sep="")
+yl <- paste("Segundo objetivo (", cual[minim[2] + 1], ")", sep="")
+png("p11_init.png")
+plot(val[,1], val[,2], xlab=xl, ylab=yl, main="Ejemplo bidimensional")
+graphics.off()
+png("p11_mejores.png")
+plot(val[,1], val[,2], xlab=paste(xl, "mejor con cuadro azul"),
+     ylab=paste(yl,"mejor con bolita naranja"),
+     main="Ejemplo bidimensional")
+points(val[mejor1, 1], val[mejor1, 2], col="blue", pch=15, cex=1.5)
+points(val[mejor2, 1], val[mejor2, 2], col="orange", pch=16, cex=1.5)
+graphics.off()
