d515d446b207e74513c34dc374ee3b2342acc152
SatuElisa <elisa.schaeffer@gmail.com>
2019-11-28 09:52:59 -0600

    Random numbers from an arbitrary distribution

---
+++ MonteCarlo/rng.R
@@ -0,0 +1,15 @@
+inicio <- -6
+final <- -inicio
+paso <- 0.25
+x <- seq(inicio, final, paso)
+f <- function(x) { return(1 / (exp(x) + exp(-x))) }
+suppressMessages(library(distr))
+g <- function(x) { return((2 / pi) * f(x)) }
+generador  <- r(AbscontDistribution(d = g)) # creamos un generador
+muestra <- generador(50000) # sacamos una muestra
+png("p5m.png") # validamos con un dibujo
+hist(muestra, freq=F, breaks=50,
+     main="Histograma de g(x) comparado con g(x)",
+     xlim=c(inicio, final), ylim=c(0, 0.4))
+lines(x, g(x), col="red") # dibujamos g(x) encima del histograma
+graphics.off()
