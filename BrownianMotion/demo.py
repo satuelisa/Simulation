0d8b52bf7c25794e0fc16ce179c5524acb6e626d
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2020-09-09 15:26:09 -0500

    Probabilidad de regreso

--- BrownianMotion/demo.py
+++ BrownianMotion/demo.py
@@ -5,9 +5,19 @@ def paso(pos, dim):
     pos[d] += -1 if random() < 0.5 else 1
     return pos
 
-dim = 50
-largo = 1000000
-pos = [0] * dim
-for t in range(largo):
-    pos = paso(pos, dim)
-print(pos)
+def experimento(largo, dim):
+    pos = [0] * dim
+    for t in range(largo):
+        pos = paso(pos, dim)
+        if all([p == 0 for p in pos]):
+            return True
+    return False
+
+dim = 1
+largo = 1000
+total = 10000
+regresos = 0
+for replica in range(total):
+    regresos += experimento(largo, dim)
+print(regresos / total)
+        
