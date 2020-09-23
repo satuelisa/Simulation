e3c9bbebf1a5924f4a6d3fa2bd4a96772ac9b6b0
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2020-09-23 15:42:23 -0500

    Bug fixes

--- CellularAutomata/gameOfLife.py
+++ CellularAutomata/gameOfLife.py
@@ -8,30 +8,41 @@ num = dim**2
 valores = [round(random()) for i in range(num)]
 actual = np.reshape(valores, (dim, dim))
 
+def mapeo(pos):
+    fila = pos // dim
+    columna = pos % dim
+    return actual[fila, columna]
+
+assert all([mapeo(x) == valores[x]  for x in range(num)])
+
 def paso(pos):
-    fila = (pos - 1) // dim
-    columna = (pos - 1) % dim
-    vecindad = actual[(fila - 1):(fila + 2), (columna - 1):(columna + 2)]
+    fila = pos // dim
+    columna = pos % dim
+    vecindad = actual[max(0, fila - 1):min(dim, fila + 2),
+                      max(0, columna - 1):min(dim, columna + 2)]
     return 1 * (np.sum(vecindad) - actual[fila, columna] == 3)
- 
-import multiprocessing
+
+print(actual)        
 if __name__ == "__main__":
     fig = plt.figure()
     plt.imshow(actual, interpolation='nearest', cmap=cm.Greys)
-    plt.savefig('p2p.png')
+    fig.suptitle('Estado inicial')
+    plt.savefig('p2_t0_p.png')
     plt.close()
     for iteracion in range(9):
         print("Iter", iteracion)
-        with multiprocessing.Pool() as pool: # rehacer para ver cambios en actual
-            valores = pool.map(paso, range(num))
-        if np.sum(valores) == 0:
-            print('Ya no queda nadie vivo.')
+        valores = [paso(x) for x in range(num)]
+        vivos = sum(valores)
+        print(iteracion, vivos)
+        if vivos == 0:
+            print('# Ya no queda nadie vivo.')
             break;
         actual = np.reshape(valores, (dim, dim))
+        print(actual)        
         fig = plt.figure()
         plt.imshow(actual, interpolation='nearest', cmap=cm.Greys)
         fig.suptitle('Paso {:d}'.format(iteracion + 1))
-        plt.savefig('p2_t{:d}_p.png'.format(iteracion))
+        plt.savefig('p2_t{:d}_p.png'.format(iteracion + 1))
         plt.close()
 # para crear un GIF, se puede usar ImageMagick con
 # convert -delay 100 -size 300x300 -loop 0 p2_t*.png p2p.gif
