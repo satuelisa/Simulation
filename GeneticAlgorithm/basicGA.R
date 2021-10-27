7c6308b6f8fc24fdc222c03807b5130934fee2fc
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2021-10-27 16:25:03 -0500

    Un for redundante eliminado

--- GeneticAlgorithm/basicGA.R
+++ GeneticAlgorithm/basicGA.R
@@ -1,27 +1,27 @@
 library(testit)
- 
+
 knapsack <- function(cap, peso, valor) {
     n <- length(peso)
-    pt <- sum(peso) 
+    pt <- sum(peso)
     assert(n == length(valor))
-    vt <- sum(valor) 
-    if (pt < cap) { 
+    vt <- sum(valor)
+    if (pt < cap) {
         return(vt)
     } else {
-        filas <- cap + 1 
-        cols <- n + 1 
+        filas <- cap + 1
+        cols <- n + 1
         tabla <- matrix(rep(-Inf, filas * cols),
-                       nrow = filas, ncol = cols) 
+                       nrow = filas, ncol = cols)
         for (fila in 1:filas) {
-            tabla[fila, 1] <- 0 
+            tabla[fila, 1] <- 0
         }
-        rownames(tabla) <- 0:cap 
+        rownames(tabla) <- 0:cap
         colnames(tabla) <- c(0, valor)
-        for (objeto in 1:n) { 
-            for (acum in 1:(cap+1)) { 
+        for (objeto in 1:n) {
+            for (acum in 1:(cap+1)) {
                 anterior <- acum - peso[objeto]
-                tabla[acum, objeto + 1] <- tabla[acum, objeto]                
-                if (anterior > 0) { 
+                tabla[acum, objeto + 1] <- tabla[acum, objeto]
+                if (anterior > 0) {
                     tabla[acum, objeto + 1] <- max(tabla[acum, objeto], tabla[anterior, objeto] + valor[objeto])
                 }
             }
@@ -29,15 +29,17 @@ knapsack <- function(cap, peso, valor) {
         return(max(tabla))
     }
 }
- 
+
+# si respeta la restriccion o no
 factible <- function(seleccion, pesos, capacidad) {
     return(sum(seleccion * pesos) <= capacidad)
 }
- 
+
+# calcular el valor total (lo que se maximiza)
 objetivo <- function(seleccion, valores) {
     return(sum(seleccion * valores))
 }
- 
+
 normalizar <- function(data) {
     menor <- min(data)
     mayor <- max(data)
@@ -45,85 +47,90 @@ normalizar <- function(data) {
     data <- data - menor # > 0
     return(data / rango) # entre 0 y 1
 }
- 
+
+# pesos normalmente distribuidos
 generador.pesos <- function(cuantos, min, max) {
     return(sort(round(normalizar(rnorm(cuantos)) * (max - min) + min)))
 }
- 
+
+# valores que dependan del peso (ojo)
 generador.valores <- function(pesos, min, max) {
     n <- length(pesos)
     valores <- double()
     for (i in 1:n) {
         media <- pesos[n]
         desv <- runif(1)
-        valores <- c(valores, rnorm(1, media, desv))
+        valores <- c(valores, rnorm(1, media, desv)) # variar esta parte
     }
     valores <- normalizar(valores) * (max - min) + min
     return(valores)
 }
- 
+
+# soluciones iniciales pseudoaleatorias
 poblacion.inicial <- function(n, tam) {
-    pobl <- matrix(rep(FALSE, tam * n), nrow = tam, ncol = n)
-    for (i in 1:tam) {
-        pobl[i,] <- round(runif(n))
-    }
+    pobl <- matrix(round(runif(tam * n)), nrow = tam, ncol = n)
     return(as.data.frame(pobl))
 }
- 
+
+# mutar la solucion sol
 mutacion <- function(sol, n) {
-    pos <- sample(1:n, 1)
-    mut <- sol
-    mut[pos] <- (!sol[pos]) * 1
-    return(mut)
+    pos <- sample(1:n, 1) # posicion por cambiar
+    mut <- sol # clon
+    mut[pos] <- (!sol[pos]) * 1 # modificar el clon
+    return(mut) # regresa el clon
 }
- 
+
+# cruzamiento: dos soluciones x & y
 reproduccion <- function(x, y, n) {
-    pos <- sample(2:(n-1), 1)
-    xy <- c(x[1:pos], y[(pos+1):n])
-    yx <- c(y[1:pos], x[(pos+1):n])
-    return(c(xy, yx))
+    pos <- sample(2:(n-1), 1) # donde cortar
+    xy <- c(x[1:pos], y[(pos+1):n]) # cabeza de x con cola de y
+    yx <- c(y[1:pos], x[(pos+1):n]) # cabeza de y con cola de x
+    return(c(xy, yx)) # regresa los hijos en un solo vector
 }
- 
-n <- 20
-pesos <- generador.pesos(n, 15, 80)
-valores <- generador.valores(pesos, 10, 500)
-capacidad <- round(sum(pesos) * 0.65)
-optimo <- knapsack(capacidad, pesos, valores)
-init <- 30
-p <- poblacion.inicial(n, init)
+
+n <- 100 # cuantos objetos
+pesos <- generador.pesos(n, 15, 80) # generar sus pesos
+valores <- generador.valores(pesos, 10, 500) # generar valores DADO los pesos
+capacidad <- round(sum(pesos) * 0.65) # capacidad 65% del peso total
+optimo <- knapsack(capacidad, pesos, valores) # cuanto vale el optimo
+print("optimo obtenido")
+init <- 50 # cuantas soluciones
+p <- poblacion.inicial(n, init) # iniciales al azar
 tam <- dim(p)[1]
 assert(tam == init)
-pm <- 0.05
-rep <- 10
-tmax <- 15
-for (iter in 1:tmax) {
+pm <- 0.1 # probabilidad de mutacion
+rep <- 10 # cantidad de cruzamientos
+tmax <- 30 # iteraciones
+for (iter in 1:tmax) { # iterar
     p$obj <- NULL
     p$fact <- NULL
     for (i in 1:tam) { # cada objeto puede mutarse con probabilidad pm
-        if (runif(1) < pm) {
-            p <- rbind(p, mutacion(p[i,], n))
+        if (runif(1) < pm) { # si es que hace
+            p <- rbind(p, mutacion(p[i,], n)) # agregar clon en la poblacion
         }
     }
     for (i in 1:rep) { # una cantidad fija de reproducciones
-        padres <- sample(1:tam, 2, replace=FALSE)
+        padres <- sample(1:tam, 2, replace=FALSE) # distintos
         hijos <- reproduccion(p[padres[1],], p[padres[2],], n)
-        p <- rbind(p, hijos[1:n]) # primer hijo
-        p <- rbind(p, hijos[(n+1):(2*n)]) # segundo hijo
+        # agregar al final del dataframe
+        p <- rbind(p, hijos[1:n]) # primer hijo (mitad)
+        p <- rbind(p, hijos[(n+1):(2*n)]) # segundo hijo (mitad)
     }
-    tam <- dim(p)[1]
+    tam <- dim(p)[1] # cuantos son ahora
     obj <- double()
     fact <- integer()
      for (i in 1:tam) {
         obj <- c(obj, objetivo(p[i,], valores))
         fact <- c(fact, factible(p[i,], pesos, capacidad))
     }
-    p <- cbind(p, obj)
-    p <- cbind(p, fact)
+    p <- cbind(p, obj) # columna de objetivo (n + 1)
+    p <- cbind(p, fact) # columna de factibilidad (n + 2)
+    # matanza
     mantener <- order(-p[, (n + 2)], -p[, (n + 1)])[1:init]
-    p <- p[mantener,]
+    p <- p[mantener,] # recortar a los primeros init
     tam <- dim(p)[1]
     assert(tam == init)
+    factibles <- p[p$fact == TRUE,] # filtrar factibles
+    mejor <- max(factibles$obj) # mayor objetivo entre factibles
+    print(paste(mejor, (optimo - mejor) / optimo))
 }
-factibles <- p[p$fact == TRUE,]
-mejor <- max(factibles$obj)
-print(paste(mejor, (optimo - mejor) / optimo))
--- GeneticAlgorithm/perfGA.R
+++ GeneticAlgorithm/perfGA.R
@@ -1,26 +1,26 @@
 library(testit)
- 
+
 knapsack <- function(cap, peso, valor) {
     n <- length(peso)
-    pt <- sum(peso) 
+    pt <- sum(peso)
     assert(n == length(valor))
-    vt <- sum(valor) 
-    if (pt < cap) { 
+    vt <- sum(valor)
+    if (pt < cap) {
         return(vt)
     } else {
-        filas <- cap + 1 
-        cols <- n + 1 
+        filas <- cap + 1
+        cols <- n + 1
         tabla <- matrix(rep(-Inf, filas * cols),
-                       nrow = filas, ncol = cols) 
+                       nrow = filas, ncol = cols)
         for (fila in 1:filas) {
-            tabla[fila, 1] <- 0 
+            tabla[fila, 1] <- 0
         }
-        rownames(tabla) <- 0:cap 
-        colnames(tabla) <- c(0, valor) 
-        for (objeto in 1:n) { 
+        rownames(tabla) <- 0:cap
+        colnames(tabla) <- c(0, valor)
+        for (objeto in 1:n) {
             for (acum in 1:(cap+1)) { # consideramos cada fila de la tabla
                 anterior <- acum - peso[objeto]
-                tabla[acum, objeto + 1] <- tabla[acum, objeto]                
+                tabla[acum, objeto + 1] <- tabla[acum, objeto]
                 if (anterior > 0) { # si conocemos una combinacion con ese peso
                     tabla[acum, objeto + 1] <- max(tabla[acum, objeto], tabla[anterior, objeto] + valor[objeto])
                 }
@@ -29,15 +29,15 @@ knapsack <- function(cap, peso, valor) {
         return(max(tabla))
     }
 }
- 
+
 factible <- function(seleccion, pesos, capacidad) {
     return(sum(seleccion * pesos) <= capacidad)
 }
- 
+
 objetivo <- function(seleccion, valores) {
     return(sum(seleccion * valores))
 }
- 
+
 normalizar <- function(data) {
     menor <- min(data)
     mayor <- max(data)
@@ -45,11 +45,11 @@ normalizar <- function(data) {
     data <- data - menor # > 0
     return(data / rango) # entre 0 y 1
 }
- 
+
 generador.pesos <- function(cuantos, min, max) {
     return(sort(round(normalizar(rnorm(cuantos)) * (max - min) + min)))
 }
- 
+
 generador.valores <- function(pesos, min, max) {
     n <- length(pesos)
     valores <- double()
@@ -61,35 +61,32 @@ generador.valores <- function(pesos, min, max) {
     valores <- normalizar(valores) * (max - min) + min
     return(valores)
 }
- 
+
 poblacion.inicial <- function(n, tam) {
-    pobl <- matrix(rep(FALSE, tam * n), nrow = tam, ncol = n)
-    for (i in 1:tam) {
-        pobl[i,] <- round(runif(n))
-    }
+    pobl <- matrix(round(runif(tam * n)), nrow = tam, ncol = n)
     return(as.data.frame(pobl))
 }
- 
+
 mutacion <- function(sol, n) {
     pos <- sample(1:n, 1)
     mut <- sol
     mut[pos] <- (!sol[pos]) * 1
     return(mut)
 }
- 
+
 reproduccion <- function(x, y, n) {
     pos <- sample(2:(n-1), 1)
     xy <- c(x[1:pos], y[(pos+1):n])
     yx <- c(y[1:pos], x[(pos+1):n])
     return(c(xy, yx))
 }
- 
+
 n <- 50
 pesos <- generador.pesos(n, 15, 80)
 valores <- generador.valores(pesos, 10, 500)
 capacidad <- round(sum(pesos) * 0.65)
 optimo <- knapsack(capacidad, pesos, valores)
-init <- 200
+init <- 30
 p <- poblacion.inicial(n, init)
 tam <- dim(p)[1]
 assert(tam == init)
