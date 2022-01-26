565b3c241c1de456ccd3297103fccd2c592258a0
Elisa Schaeffer <elisa.schaeffer@gmail.com>
2022-01-26 13:16:43 -0600

    Ahora mide memoria

--- Homework0/medir.R
+++ Homework0/medir.R
@@ -1,4 +1,6 @@
+library(pryr)
 for (n in 9:14) {
     k = 2^n
-    cat(k, system.time(matrix(runif(k*k), nrow=k))[3], '\n')
+    cat('tiempo', k, system.time(matrix(runif(k*k), nrow=k))[3], '\n')
+    cat('memoria', k, object_size(matrix(runif(k*k), nrow=k)), '\n')
 }
